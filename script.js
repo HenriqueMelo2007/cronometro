4a2901e80ded6a87aa3ec8b44effad7cfba61d41
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2022-09-29 11:02:18 -0300

    bringing timer values and HTML buttons to JavaScript

---
+++ script.js
@@ -0,0 +1,8 @@
+let horas = document.querySelector('#number-horas')
+let minutos = document.querySelector('#number-minutos')
+let segundos = document.querySelector('#number-segundos')
+let decimos = document.querySelector('#number-decimos')
+
+const comecar = document.querySelector('#btn-comecar')
+const parar = document.querySelector('#btn-parar')
+const zerar = document.querySelector('#btn-zerar')
