254942cd210e144d9a7ac19a82de4698e50a18c8
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2022-09-29 14:51:09 -0300

    completion of the entire stopwatch system

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
+// Variáveis das unidades de tempo do cronometro
 let decimos = 0
 let segundos = 0
 let minutos = 0
 let horas = 0
 
+// intervalo
+let interval
+
+// botão "começar" e evento de click
 const comecar = document.querySelector('#btn-comecar')
-const parar = document.querySelector('#btn-parar')
-const zerar = document.querySelector('#btn-zerar')
+comecar.addEventListener('click', comecarCounter)
 
-comecar.addEventListener('click', comecarCC)
-
-function comecarCC () {
-  setInterval(counterDecimosNN, 100)
+// função que define o intervalo do contador
+function comecarCounter () {
+  interval = setInterval(counterDecimosNN, 100)
 }
 
+// função que faz a contagem
 function counterDecimosNN () {
   decimos++
 
+  // trazendo o HTML para o script e atribuindo a variável de tempo que será incrementada
   document.querySelector('#number-decimos').innerHTML = decimos
   document.querySelector('#number-segundos').innerHTML = segundos
   document.querySelector('#number-minutos').innerHTML = minutos
   document.querySelector('#number-horas').innerHTML = horas
 
+  // contagem
   if (decimos == 10) {
     decimos = 0
     segundos++
-  }
-
-  if (decimos < 10) {
-    document.querySelector('#number-decimos').innerHTML = `0${decimos}`
   }
 
   if (segundos == 60) {
@@ -35,13 +37,18 @@ function counterDecimosNN () {
     minutos++
   }
 
-  if (segundos < 10) {
-    document.querySelector('#number-segundos').innerHTML = `0${segundos}`
-  }
-
   if (minutos == 60) {
     minutos = 0
     horas++
+  }
+
+  // organizção visual do HTML
+  if (decimos < 10) {
+    document.querySelector('#number-decimos').innerHTML = `0${decimos}`
+  }
+
+  if (segundos < 10) {
+    document.querySelector('#number-segundos').innerHTML = `0${segundos}`
   }
 
   if (minutos < 10) {
@@ -51,4 +58,31 @@ function counterDecimosNN () {
   if (horas < 10) {
     document.querySelector('#number-horas').innerHTML = `0${horas}`
   }
+}
+
+// botão "pausar" e evento de click
+const pausar = document.querySelector('#btn-pausar')
+pausar.addEventListener('click', pausarCounter)
+
+// função que faz a pausa
+function pausarCounter () {
+  clearInterval(interval)
+}
+
+// botão "zerar" e evento de click
+const zerar = document.querySelector('#btn-zerar')
+zerar.addEventListener('click', zerarCounter)
+
+// função que zera o cronômetro
+function zerarCounter () {
+  clearInterval(interval)
+  decimos = 0
+  segundos = 0
+  minutos = 0
+  horas = 0
+
+  document.querySelector('#number-decimos').innerHTML = `0${decimos}`
+  document.querySelector('#number-segundos').innerHTML = `0${segundos}`
+  document.querySelector('#number-minutos').innerHTML = `0${minutos}`
+  document.querySelector('#number-horas').innerHTML = `0${horas}`
 }
