6aa815a75650350410db16f048b407ae49bb1394
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2022-09-29 12:41:11 -0300

    init JavaScript

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-let horas = document.querySelector('#number-horas')
-let minutos = document.querySelector('#number-minutos')
-let segundos = document.querySelector('#number-segundos')
-let decimos = document.querySelector('#number-decimos')
+let decimos = 00
 
 const comecar = document.querySelector('#btn-comecar')
 const parar = document.querySelector('#btn-parar')
 const zerar = document.querySelector('#btn-zerar')
+
+comecar.addEventListener('click', comecarCC)
+
+function comecarCC () {
+  setInterval(counterDecimosNN, 100)
+}
+
+function counterDecimosNN () {
+  decimos++
+  document.querySelector('#number-decimos').innerHTML = decimos
+}
